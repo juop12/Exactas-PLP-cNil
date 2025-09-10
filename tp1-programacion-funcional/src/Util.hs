4b0c122c0a4e2975c851ddf693a881c5aa10f49c
juop12 <sadeijuop12@gmail.com>
2025-09-10 22:08:30 +0200

    refactor: Cambio calcularPorcentaje

--- tp1-programacion-funcional/src/Util.hs
+++ tp1-programacion-funcional/src/Util.hs
@@ -3,20 +3,20 @@ module Util where
 -- | @alinearDerecha n s@ agrega espacios a la izquierda de @s@ hasta que su longitud sea @n@.
 -- Si @s@ ya tiene longitud @>= n@, devuelve @s@.
 alinearDerecha :: Int -> String -> String
-alinearDerecha indice s = 
-    let cantEspacios = indice - length s in 
-        if cantEspacios <= 0
-            then s 
-            else [' ' | c <- [1..cantEspacios]] ++ s
+alinearDerecha indice s =
+  let cantEspacios = indice - length s
+   in if cantEspacios <= 0
+        then s
+        else [' ' | c <- [1 .. cantEspacios]] ++ s
 
 -- | Dado un índice y una función, actualiza el elemento en la posición del índice
 -- aplicando la función al valor actual. Si el índice está fuera de los límites
 -- de la lista, devuelve la lista sin cambios.
 -- El primer elemento de la lista es el índice 0.
 actualizarElem :: Int -> (a -> a) -> [a] -> [a]
-actualizarElem n f = zipWith aplicaEnIndice [0..]
-    where
-        aplicaEnIndice p1 p2 = if n == p1 then f p2 else p2
+actualizarElem n f = zipWith aplicaEnIndice [0 ..]
+  where
+    aplicaEnIndice p1 p2 = if n == p1 then f p2 else p2
 
 -- | infinito positivo (Haskell no tiene literal para +infinito)
 infinitoPositivo :: Float
@@ -26,13 +26,9 @@ infinitoPositivo = 1 / 0
 infinitoNegativo :: Float
 infinitoNegativo = -(1 / 0)
 
-
-cantTotalDeNumeros :: [Int] -> Int
-cantTotalDeNumeros = foldl1 (+)
-
 porcentaje :: Int -> Int -> Float
 porcentaje _ 0 = 0
-porcentaje x y = (fromIntegral x/fromIntegral y) * 100
+porcentaje x y = (fromIntegral x / fromIntegral y) * 100
 
 calcularPorcentajes :: [Int] -> [Float]
-calcularPorcentajes xs = map (\x -> porcentaje x (cantTotalDeNumeros xs)) xs
+calcularPorcentajes xs = map (\x -> porcentaje x (sum xs)) xs
